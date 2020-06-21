fff7c087c7c610cf6eabef2252e7b4756b603c21
DimitrisZar <dimitriszarahanis@gmail.com>
2020-06-22 00:13:13 +0300

    remove custom decimal cutting fn in favor of js native toFixed()

--- Randjs.js
+++ Randjs.js
@@ -1,17 +1,5 @@
 const { floor, random } = Math;
 
-// Removes decimal points from a floating point number
-const cutDecimals = (float, howMany) => {
-  /* 
-    TODO:
-      [IMPORTANT] handle rounding last remaining decimal if last cut decimal is >= 5 
-  */
-  const [intPart, floatPart] = String(float).split('.');
-  if (howMany >= floatPart.length) return intPart; // if howMany is larger than the number of decimals return just the integer part.
-  const newfloatPart = floatPart.slice(0, -howMany);
-  return `${intPart}.${newfloatPart}`;
-}
-
 // Return a random int / optional: limits
 const randInt = limits => limits.length === 1
   ? floor(random() * (limits[0] + 1))
@@ -24,7 +12,7 @@ const randFloat = (limits, precision = null) => {
     : (random() * limits[1]) + limits[0];
 
   return precision !== null
-    ? cutDecimals(randomFloat, precision)
+    ? randomFloat.toFixed(precision)
     : randomFloat;
 }
 
@@ -33,8 +21,7 @@ const randItem = list => list[randInt([0, list.length])];
 
 
 // Tests | Should implement testing through a library later
-console.log(cutDecimals(1.1251, 4));
 console.log(randInt([5]));
 console.log(randInt([5, 99]));
-console.log('randfloat', randFloat([5, 99], 5));
+console.log('randfloat', randFloat([5, 99], 1));
 console.log(randItem(['a', 'abcda', '12341']));
